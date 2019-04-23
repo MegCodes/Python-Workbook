4c8573d88b3a89d5835d8e75b2e8ec7ca6beb871
Donna Dia <0dvd0bvb0@gmail.com>
2019-04-23 12:22:30 +0300

    #1 revised + Unittest. 23.04.2019

--- Mailing_Address.py
+++ Mailing_Address.py
@@ -2,15 +2,10 @@ class Addressee():
 
     def __init__(self, addressee, street, box, city, postal_code, country):
         self.addressee = addressee.title()
-        self.street = street.title()
-        self.box = str(box)
-        self.city = city.title()
-        self.postal_code = str(postal_code)
+        self.street = street.title() + f', {str(box)}'
+        self.city = city.title() + f', {str(postal_code)}'
         self.country = country.upper()
 
     def mailing_address(self):
-        formatted_address = self.addressee, self.street, self.box, self.city, self.postal_code, self.country
-        for element in formatted_address:
-            print(element)
-
-
+        formatted_address = f"{self.addressee}\n{self.street}\n{self.city}\n{self.country}"
+        return formatted_address
